a4ac2594295c3f3d9717964c8c869f026ec677b9
Jujungin <wjddls0119@gmail.com>
2024-02-23 13:19:52 +0900

    Q002

--- euler_archives/Q002.py
+++ euler_archives/Q002.py
@@ -32,26 +32,26 @@ def fibonacci_02(require):
     return sum_of_even_number
     pass
 
-# def fibonacci_03(require):
-#     def fibonacci_recursive(n):
-#         if n <= 1:
-#             return n
-#         else:
-#             return fibonacci_recursive(n-1) + fibonacci_recursive(n-2)
+def fibonacci_03(require):
+    def fibonacci_recursive(n):
+        if n <= 1:
+            return n
+        else:
+            return fibonacci_recursive(n-1) + fibonacci_recursive(n-2)
     
-#     sum_of_even_number = 0
-#     n = 0
-#     fib = 0
-#     while fib <= require:
-#         fib = fibonacci_recursive(n)
-#         if fib % 2 == 0:
-#             sum_of_even_number += fib
-#         n += 1
+    sum_of_even_number = 0
+    n = 0
+    fib = 0
+    while fib <= require:
+        fib = fibonacci_recursive(n)
+        if fib % 2 == 0:
+            sum_of_even_number += fib
+        n += 1
     
-#     return sum_of_even_number
+    return sum_of_even_number
         
 
-def measure_average_time(func, arg, num_trials=1000):
+def measure_average_time(func, arg, num_trials=1):
     total_time = 0
     for _ in range(num_trials):
         start_time = time.time()
@@ -60,11 +60,11 @@ def measure_average_time(func, arg, num_trials=1000):
         total_time += end_time - start_time
     return total_time / num_trials
 
-require_list = [10, 100, 1000, 10000, 100000, 1000000, 4000000]
+require_list = [10, 100, 1000, 10000, 100000, 1000000, 10000000]
 # require_list = [10, 100, 1000]
 time_list_01 = []
 time_list_02 = []
-# time_list_03 = []
+time_list_03 = []
 
 for require in require_list:
     time_01 = measure_average_time(fibonacci_01, require)
@@ -73,18 +73,18 @@ for require in require_list:
     time_02 = measure_average_time(fibonacci_02, require)
     time_list_02.append(time_02)
     
-    # time_03 = measure_average_time(fibonacci_03, require)
-    # time_list_03.append(time_03)
+    time_03 = measure_average_time(fibonacci_03, require)
+    time_list_03.append(time_03)
 
 
 
 plt.plot(require_list, time_list_01, color='b', linestyle='-', linewidth=5, label='Function 01')
 plt.plot(require_list, time_list_02, color='r', linestyle='--', linewidth=2, label='Function 02')
-# plt.plot(require_list, time_list_03, color='y', linestyle='-.', linewidth=3, label='Function 03')
+plt.plot(require_list, time_list_03, color='y', linestyle='-.', linewidth=3, label='Function 03')
 plt.xlabel('require', fontsize=10)
 plt.ylabel('Time (s)', fontsize=10)
 plt.title('Time Complexity', fontsize=12)
-# plt.xticks(require_list, ['{:,.0f}'.format(num) for num in require_list], fontsize=10)
+plt.xticks(require_list, ['{:,.0f}'.format(num) for num in require_list], fontsize=10)
 # plt.xticks(require_list, ['{:,.0e}'.format(num) for num in require_list], fontsize=5)
 plt.xticks(fontsize=8)
 plt.yticks(fontsize=8)
